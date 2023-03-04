0374714953ad21b22e53a39c2fc722fcbab4abba
JorVLP <47005135+JorVLP@users.noreply.github.com>
2023-03-04 12:35:03 +0000

    poetry init

---
+++ test.py
@@ -0,0 +1,16 @@
+from gpiozero import PWMLED
+from gpiozero.pins.pigpio import PiGPIOFactory
+from time import sleep
+import gpiozero.spi_devices
+gpiozero.spi_devices
+
+factory = PiGPIOFactory(host="192.168.62.107")
+# pins http://abyz.me.uk/rpi/pigpio/index.html#Type_3
+# ST7735
+# https://www.youtube.com/watch?v=SYdGNpfLxKw
+# for i in range(1,30):
+blue = PWMLED(2, pin_factory=factory)
+
+blue.on()
+sleep(1)
+blue.value = 0.01
